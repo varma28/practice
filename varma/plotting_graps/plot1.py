2a6a70e1e8be6a45f124772e95dbba5b46fa001d
varma28 <vroopa19@gmail.com>
2022-06-07 15:55:08 +0530

    Practice

---
+++ varma/plotting_graps/plot1.py
@@ -0,0 +1,21 @@
+import math
+str1 = 'varma'
+
+a = 34.95
+print(math.floor(a),round(a))
+print(math.trunc(a))
+
+str2 = 2
+
+str3 = str1*str2
+
+#print(str1.getItem(3))
+
+k = 2+3j
+l = 2+3J
+op = complex(2,3)
+print(k,l,op)
+
+print((0.1 + 0.2) == 0.3)
+a1 = math.inf(23)
+print(a1)
---
+++ varma/plotting_graps/test.py
@@ -0,0 +1,11 @@
+stre1 = input()
+
+strnew = stre1[-1:-1*(len(stre1)+1):-1]
+
+#strnew = stre1[::-1]
+
+print("The reverse of the string is",strnew)
+
+
+
+
